f4d7948e46ac4c1680db4e08cad75bfe91e9adfa
pchchv <ipchchv@gmail.com>
2023-04-22 23:53:40 +0300

    Implement for the area of various shapes

---
+++ math/area.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * @brief Implementations for the area of various shapes.
+ * Reference: https://en.wikipedia.org/wiki/Area
+ * @details The area of a shape is the amount of 2D space it takes up.
+ * All shapes have a formula to get the area of any given shape.
+ * These implementations support multiple return types.
+ */
+#define _USE_MATH_DEFINES
+#include <cassert>  /// for assert
+#include <cmath>    /// for M_PI definition and pow()
+#include <cmath>
+#include <cstdint>   /// for uint16_t datatype
+#include <iostream>  /// for IO operations
+
+/**
+ * @namespace math
+ * @brief Mathematical algorithms
+ */
+namespace math {
+}  // namespace math
